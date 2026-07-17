8b96cae3248cf423b0cb78d4ca619e6714f2b3b9
bhuvizara282006-oss <bhuvizara282006@gmail.com>
2026-07-17 20:16:11 +0530

    Add variable demonstration in variables_demo.py

---
+++ variables_demo.py
@@ -0,0 +1,20 @@
+i = 10
+print(i)
+print(type(i))
+
+j = 5.4
+print(j)
+print(type(j))
+
+B = "kav"
+print(B)
+print(type(B))
+
+ch = 'B'
+print(ch)
+print(type(ch))
+
+x = 10
+y = 15
+s = x + y
+print(s)
